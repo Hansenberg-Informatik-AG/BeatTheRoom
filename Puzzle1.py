aa84f030ab863f7c78604cb59d77fe3d1127ac1c
watertrainer <47402928+watertrainer@users.noreply.github.com>
2020-05-26 18:27:16 +0200

    Puzzle mit Kommentaren

--- Puzzle1.py
+++ Puzzle1.py
@@ -1,21 +1,30 @@
 
+import time
+import beat_the_room_single
 
-
-from beat_the_room import Puzzle
-
-class Puzzle1(Puzzle):
+class Puzzle1(beat_the_room_single.Puzzle):
 
     def init(self):
-        self.hints = [beat_the_room.make_hint(self, "test.avi")]
-        self.activates_ids = [1,4,6]
+#hinweis filmdatei
+# die Rätsel sind linear
+        self.hints = [beat_the_room_single.make_hint(self, "test.avi")]
+       
         self.id = 42
+        print("Init(42)")
+
+
+
 
         #INITIALISIERE SENSOREN / HARDWARE
         
     def interact(self):
-        while not BEDINGUNG:
-            #TUE ETWAS
+        print("interacting(42)")
+        time.sleep(10)
+#sobald diese variable gesetzt ist, ist das Rätsel fertig! Hier muss wahrscheinlich immer eine while Schleife rein!
         self.solved = True
+        print("Fertig(42)")
 
     def deinit(self):
+        print("deinitlasing(42)")
         #DEINITIALISIERE SENSOREN / HARDWARE
+
