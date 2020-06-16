f0957fed085267382067057f285602edcba0be12
watertrainer <47402928+watertrainer@users.noreply.github.com>
2020-06-16 19:44:14 +0200

    Update startMaschine.py

--- startMaschine.py
+++ startMaschine.py
@@ -18,21 +18,13 @@ class Puzzle1(beat_the_room.Puzzle):
         
         #Display zunächst ausgeschaltet, bzw dunkel
         self.hints = [beat_the_room.make_hint(self, "test.avi"),beat_the_room.make_hint(self,"hinweis2.avi")]
-        #self.id = 01
-        #print("Init(01)")
-
-
+        self.id = 01
+        print("Init(01)")
         gpio.setmode(gpio.BCM)
+     
+        
 
-        matrix = [['1','2','3','A'],
-            ['4','5','6','B'],
-            ['7','8','9','C'],
-            ['*', '0', '#','D']]
-
-        spalte = [16, 20, 21,12]
-        zeile = [6, 13, 19, 26]
-
-        for j in range(len(spalte)):
+      
             gpio.setup(spalte[j], gpio.OUT)
             gpio.output(spalte[j], 1)
 
@@ -42,27 +34,20 @@ class Puzzle1(beat_the_room.Puzzle):
         #INITIALISIERE SENSOREN / HARDWARE
         
     def interact(self):
-        print("interacting(42)")
-        lastNumbers = [0,0,0,0]
-        while True:
-            for j in range(3):
-                gpio.output(spalte[j], 0)
-                for i in range(4):
-                    if gpio.input(zeile[i]) == 0:
-                        benutzerEingabe = matrix[i][j]
-                        lastNumbers.pop(0)
-                        lastNumbers.append(benutzerEingabe)
-                        while gpio.input(zeile[i]) == 0:
-                            pass
-                        return benutzerEingabe                  
-                gpio.output(spalte[j], 1)
+        print("interacting(01)")
+        self.solved = False
+        while not self.solved:
+            if gpio.input() ==1:
+                self.solved = True
+                print("Fertig(01)")
+               
+               
         #time.sleep(10)
         #sobald diese variable gesetzt ist, ist das Rätsel fertig! Hier muss wahrscheinlich immer eine while Schleife rein!
-        self.solved = True
-        print("Fertig(42)")
+       
 
     def deinit(self):
-        print("deinitlasing(42)")
+        print("deinitlasing(01)")
         #DEINITIALISIERE SENSOREN / HARDWARE
         #GPIO.cleanup() etc.
 
