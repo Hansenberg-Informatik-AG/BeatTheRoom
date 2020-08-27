d47b1187c5f9217c8fbeb90a56aba746f9800d65
fpwg <46819544+fpwg@users.noreply.github.com>
2020-08-27 18:38:00 +0200

    blubb

--- fm.py
+++ fm.py
@@ -3,15 +3,13 @@ import subprocess
 
 process = None
 
-
-
 def start_radio():
 	global process
 	process = subprocess.Popen(["/home/pi/FM/FM_Transmitter_RPi3/fm_transmitter", "-f", "107.3", "/home/pi/FM/FM_Transmitter_RPi3/Test_44_16.wav"])
-
 
 def terminate():
 	process.terminate()
 	process.kill()
 
-start_radio()
+if __name__=="__main__":
+	start_radio()
