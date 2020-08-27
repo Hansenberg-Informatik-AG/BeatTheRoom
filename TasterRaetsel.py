f56c95f2e0dbc02b7d854f8c18d821f2168803c4
Jona Rumberg <jona@rumberg-online.de>
2020-08-27 19:42:29 +0200

    basisfunktionalität hinzugefügt

--- TasterRaetsel.py
+++ TasterRaetsel.py
@@ -1,4 +1,5 @@
 
+from beat_the_room import Puzzle
 import RPi.GPIO as GPIO
 
 
@@ -11,6 +12,7 @@ class TasterRaetsel(beat_the_room.Puzzle):
         self.states = [False for i in range(4)]
         self.clickCounts = [0 for i in range(4)]
         self.currentPin = 0
+        self.key = [3,5,3,1]
         #reservier mal paar pins
 
     def interact(self):
@@ -18,14 +20,24 @@ class TasterRaetsel(beat_the_room.Puzzle):
             pin = checkClicks()
             if pin != -1:
                 if pin == self.currentPin:
-                    self.clickCounts[pin] += 1
-                elif if pin == self.currentPin + 1:
+                    self.click()
+                elif pin == self.currentPin + 1:
                     self.currentPin += 1
-                    self.clickCounts[self.currentPin]
+                    self.click()
+                elif pin == 0:
+                    self.currentPin = 0
+                    self.clickCounts = [0 for i in range(4)]
+                    self.click()
 
     def deinit(self):
         pass
 
+    def click(self):
+        self.clickCounts[self.currentPin] += 1
+        self.solved = self.isSolved()
+
+    def isSolved(self):
+        return self.clickCounts == self.key
 
     def checkClicks(self):
         for i in range(4):
