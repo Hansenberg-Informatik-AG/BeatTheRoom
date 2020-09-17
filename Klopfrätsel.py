5929067fc23b8f9a69049e226a028b67ebb7f3ee
watertrainer <watertrainer42@gmail.com>
2020-09-17 18:31:01 +0200

    Added Testin

--- Klopfrätsel.py
+++ Klopfrätsel.py
@@ -6,24 +6,25 @@ import RPi.GPIO as GPIO
 class Puzzle1(beat_the_room.Puzzle):
 
     def init(self):
-        #Annika und Johann Versuch  zum Klopfrätsel
-        #Sollte einmaliges Klopfen erkennen
+        # Annika und Johann Versuch  zum Klopfrätsel
+        # Sollte einmaliges Klopfen erkennen
         # hinweis filmdatei
         # die Rätsel sind linear
         self.hints = [beat_the_room.make_hint(
             self, "test.avi"), beat_the_room.make_hint(self, "hinweis2.avi")]
         # print("Init(42)")
         # INITIALISIERE SENSOREN / HARDWARE
-        global GPIO_PIN=24
+        global GPIO_PIN = 24
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(GPIO_PIN, GPIO.IN)
 
     def interact(self):
         print("interacting(42)")
-        GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=self.lösen, bouncetime=1000)
+        GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING,
+                              callback=self.lösen, bouncetime=1000)
         # time.sleep(10)
         # sobald diese variable gesetzt ist, ist das Rätsel fertig! Hier muss wahrscheinlich immer eine while Schleife rein!
-        while self.solved == False :
+        while self.solved == False:
             time.sleep(1)
 
         print("Fertig(42)")
@@ -35,3 +36,12 @@ class Puzzle1(beat_the_room.Puzzle):
         print("deinitlasing(42)")
         # DEINITIALISIERE SENSOREN / HARDWARE
         GPIO.cleanup()
+
+
+test = True
+if test:
+    Puzzle1.init()
+    time.sleep(1)
+    Puzzle1.interact()
+    print("succsess")
+    Puzzle1.deinit()
