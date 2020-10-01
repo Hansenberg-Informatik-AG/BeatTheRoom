139d55fc80106ed52856fffba89c12b9604602f3
annika-krg <66421212+annika-krg@users.noreply.github.com>
2020-10-01 18:31:03 +0200

    Update Klopfrätsel.py

--- Klopfrätsel.py
+++ Klopfrätsel.py
@@ -24,7 +24,7 @@ class Puzzle1(beat_the_room.Puzzle):
     def interact(self):
         print("interacting(15)")
         GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING,
-                              callback=self.lösen, bouncetime=1)
+                              callback=self.lösen, bouncetime=3000)
         # time.sleep(10)
         # sobald diese variable gesetzt ist, ist das Rätsel fertig! Hier muss wahrscheinlich immer eine while Schleife rein!
         while self.solved == False:
