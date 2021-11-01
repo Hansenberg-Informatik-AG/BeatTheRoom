1d3e37181838be8012bb1c8510349e3e620096c3
annika-krg <66421212+annika-krg@users.noreply.github.com>
2021-11-01 18:02:49 +0100

    Update beat_the_room.py

--- beat_the_room.py
+++ beat_the_room.py
@@ -18,8 +18,8 @@ class Controller(object):
         # puzlle hier Importieren und zu der Liste hinzufuegen
 # die Reihenfolge hier ist auch die Reihenfolge der Puzzle!
         #self.puzzles.append(NumpadRaetsel.NumpadRaetsel())
-        #self.puzzles.append(SchluessekRaetsel.SchluesselRaetsel())
-        #self.puzzles.append(KameraRaetsel.KameraRaetsel()) # TODO: Wieder aufrufen
+        self.puzzles.append(SchluessekRaetsel.SchluesselRaetsel())
+        self.puzzles.append(KameraRaetsel.KameraRaetsel()) # TODO: Wieder aufrufen
         self.puzzles.append(Klopfraetsel.Klopfraetsel())
         #self.puzzles.append(Puzzle1.Puzzle1())
         #self.puzzles.append(RadioRaetsel.RadioRaetsel())
