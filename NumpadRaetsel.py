ccc430a9addbf0386dd3efcc63fa831c31594702
Sebastian Thomas <kontakt@sthomas.ch>
2021-11-08 19:06:17 +0100

    Fixed self.-error

--- NumpadRaetsel.py
+++ NumpadRaetsel.py
@@ -33,7 +33,7 @@ class NumpadRaetsel(beat_the_room.Puzzle):
 
         self.factory = rpi_gpio.KeypadFactory()
         #self.keypad = self.factory.create_keypad(keypad=self.matrix, row_pins=self.zeile, col_pins=self.spalte)
-        self.keypad = factory.create_4_by_4_keypad
+        self.keypad = self.factory.create_4_by_4_keypad
         
         self.keypad.registerKeyPressHandler(self.printKey)
 
