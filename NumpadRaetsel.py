fac3734a2cc4d3dd6146ab9a709c69bd1bca53f4
Sebastian Thomas <kontakt@sthomas.ch>
2021-12-06 17:59:55 +0100

    Pad reagiert nicht

--- NumpadRaetsel.py
+++ NumpadRaetsel.py
@@ -36,11 +36,6 @@ class NumpadRaetsel(beat_the_room.Puzzle):
           
           char = -1
           benutzerEingabe = -1
-          
-          for j in range(4):
-              if (gpio.input(self.zeile[j]) == 1): 
-                  print("Input " + str(j) + str(gpio.input(self.zeile[j])))
-                  print("-------------------------------------------------------")
                 
           if gpio.input(self.zeile[0]) == 1:
               char = 0
